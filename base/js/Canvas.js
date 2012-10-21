421a48aff35e98d8704b096f6d5b335dd0db5643
Chris Allick <chrisallick@gmail.com>
2012-10-20 18:25:59 -0700

    ok, got base example.

---
+++ base/js/Canvas.js
@@ -0,0 +1,51 @@
+Canvas = function( _width, _height, _target, _autostart ) {
+    var self = this;
+    
+    this.t = _target;
+    this.w = _width;
+    this.h = _height;
+    this.auto = _autostart;
+
+    this.canvas;
+    this.context;
+
+    this.setup = function() {
+        self.canvas = document.createElement( 'canvas' );
+        self.canvas.id = "canvas";
+        self.canvas.width = self.w;
+        self.canvas.height = self.h;
+
+        self.context = self.canvas.getContext( '2d' );
+
+        self.t.append( self.canvas );
+            
+        $(self.canvas).hide().fadeIn(1000);
+        if( self.auto ) {
+            self.animate();
+        }
+    }
+
+    this.draw = function() {
+        var time = new Date().getTime() * 0.002;
+        var x = Math.sin( time ) * 96 + 128;
+        var y = Math.cos( time * 0.9 ) * 96 + 128;
+
+        self.context.fillStyle = 'rgb(245,245,245)';
+        self.context.fillRect( 0, 0, 255, 255 );
+
+        self.context.fillStyle = 'rgb(255,0,0)';
+        self.context.beginPath();
+        self.context.arc( x, y, 10, 0, Math.PI * 2, true );
+        self.context.closePath();
+        self.context.fill();
+    }
+
+    this.animate = function() {
+        requestAnimFrame( self.animate );
+        self.draw();
+    }
+
+    this.start = function() {
+        self.setup();
+    }   
+}
--- base/js/main.js
+++ base/js/main.js
@@ -1,34 +1,5 @@
-draw = function() {
-    var time = new Date().getTime() * 0.002;
-    var x = Math.sin( time ) * 96 + 128;
-    var y = Math.cos( time * 0.9 ) * 96 + 128;
-
-    context.fillStyle = 'rgb(245,245,245)';
-    context.fillRect( 0, 0, 255, 255 );
-
-    context.fillStyle = 'rgb(255,0,0)';
-    context.beginPath();
-    context.arc( x, y, 10, 0, Math.PI * 2, true );
-    context.closePath();
-    context.fill();
-}
-
-animate = function() {
-    requestAnimFrame( animate );
-    draw();
-}
-
-var canvas, context;
+var example;
 $(document).ready(function() {
-    canvas 	= document.createElement( 'canvas' );
-    canvas.id = "canvas";
-    canvas.width = 256;
-    canvas.height = 256;
-
-    context = canvas.getContext( '2d' );
-
-    $("#wrapper").append( canvas );
-        
-    $("#canvas").hide().fadeIn(1000);
-    animate();
+	example = new Canvas( 640, 480, $("#wrapper"), true );
+	example.start();
 });
