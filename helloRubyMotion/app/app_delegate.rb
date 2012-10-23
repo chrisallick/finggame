593801f66f815cc0a0f4d60bdd3fcba341193795
Chris Allick <chrisallick@gmail.com>
2012-10-23 12:31:18 -0700

    lulz

--- helloRubyMotion/app/app_delegate.rb
+++ helloRubyMotion/app/app_delegate.rb
@@ -1,7 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     alert = UIAlertView.new
-    alert.message = "Hello World!"
+    alert.message = "You think this a mother fucking game?!"
     alert.show
     true
   end
