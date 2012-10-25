e376f38c81ff3cf42f027856873f9aca86acbdac
Chris Allick <chrisallick@gmail.com>
2012-10-25 16:14:42 -0700

    added trash url for chat.

--- multiplayerChaseAndEat/js/TrashIO.js
+++ multiplayerChaseAndEat/js/TrashIO.js
@@ -7,7 +7,8 @@ TrashIO = function( _p, _room ) {
     this.room = _room;
 
     //this.ws_url = "ws://" + window.location.hostname + ":8081/" + parent.room;
-    this.ws_url = "ws://localhost:8081/" + self.room;
+    //this.ws_url = "ws://localhost:8081/" + self.room;
+    this.ws_url = "ws://stage.trash.io:8081/" + self.room;
 
     this.onWelcome = function(sid,players) {
         for( var i in players ) {
