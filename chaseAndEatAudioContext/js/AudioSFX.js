bfc44b7d92665c32295f475963c18a1eeeac4147
Chris Allick <chrisallick@gmail.com>
2012-10-31 15:43:52 -0700

    removed comments

--- chaseAndEatAudioContext/js/AudioSFX.js
+++ chaseAndEatAudioContext/js/AudioSFX.js
@@ -1,37 +1,34 @@
 AudioSFX = function( _p, _autoplay ) {
-	var self = this;
-	var parent = _p;
+    var self = this;
+    var parent = _p;
 
-	this.sounds = new Array();
+    this.sounds = new Array();
 
-	this.context = new webkitAudioContext();
-	this.autoplay = _autoplay;
-
-    // sfx = new Audio("wav/sfx.wav");
-    // http://f1lt3r.com/w3caudio/web-audio-api/basic-examples/looping-sounds-without-gaps.html
+    this.context = new webkitAudioContext();
+    this.autoplay = _autoplay;
 
     this.play = function( sound ) {
-    	var source = self.context.createBufferSource();
-    	source.buffer = self.sounds[sound];
-    	source.connect( self.context.destination );
-    	source.noteOn( 0 );
+        var source = self.context.createBufferSource();
+        source.buffer = self.sounds[sound];
+        source.connect( self.context.destination );
+        source.noteOn( 0 );
     }
 
     this.load = function() {
-    	var request = new XMLHttpRequest();
-    	request.addEventListener( 'load', function(e) {
-        	self.context.decodeAudioData( request.response, function(decoded_data) {
-        		self.sounds[0] = decoded_data;
-        		if( self.autoplay ) {
-        			self.play(0);
-        		}
-        	}, function(e){
-            	console.log("error");
-        	});
-    	}, false);
-    	request.open( 'GET', 'wav/sfx.wav', true );
-    	request.responseType = "arraybuffer";
-    	request.send();
+        var request = new XMLHttpRequest();
+        request.addEventListener( 'load', function(e) {
+            self.context.decodeAudioData( request.response, function(decoded_data) {
+                self.sounds[0] = decoded_data;
+                if( self.autoplay ) {
+                    self.play(0);
+                }
+            }, function(e){
+                console.log("error");
+            });
+        }, false);
+        request.open( 'GET', 'wav/sfx.wav', true );
+        request.responseType = "arraybuffer";
+        request.send();
     }
 
     self.load();
