1a3e818c09b0a8c40aef57cbce15ac46cbc0a6fd
Chris Allick <chrisallick@gmail.com>
2012-10-31 14:11:04 -0700

    Testing out using a audio context instead of audio tag in order to get audio on canvas for iOS 6

---
+++ chaseAndEatAudioContext/js/Food.js
@@ -0,0 +1,47 @@
+Food = function( _parent, _x, _y, _width, _height, _value ) {
+	var self = this;
+	var parent = _parent;
+
+	this.x = _x;
+	this.y = _y;
+	this.w = _width;
+	this.h = _height;
+	this.alive = true;
+	this.last_time = 0;
+	this.step = 0;
+
+	this.draw = function(timestamp) {
+		if( self.alive ) {
+			parent.context.fillStyle = "rgb(150,29,28)";
+			parent.context.fillRect( self.x, self.y, self.w, self.h );
+
+	        if( !self.last_time ) {
+	            self.last_time = timestamp;
+	        } else {
+	            if( timestamp - self.last_time > 500 && parent.active) {
+	                self.step++;
+	                self.last_time = timestamp;
+	            }
+	        }
+
+	        if( self.step > 3 ) {
+	        	self.step = 0;
+	        	self.w = self.h = getRandInt( 8, 30 );
+	        	self.x = getRandInt(0,parent.w-self.w);
+	        	self.y = getRandInt(0,parent.h-self.h);
+	        }
+		}
+
+	}
+	
+	this.hitTest = function( character ) {
+        //console.log( self.x, self.y, character.x, character.y );
+		if( self.x >= character.x && self.x <= character.x+character.w || self.x+self.w >= character.x && self.x+self.w <= character.x+character.w ) {
+			if( self.y >= character.y && self.y <= character.y+character.h || self.y+self.h >= character.y && self.y+self.h <= character.y+character.h ) {
+				self.alive = false;
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ chaseAndEatAudioContext/js/main.js
@@ -0,0 +1,56 @@
+var example;
+var audiocontext, audiosource;
+$(document).ready(function() {
+    /*
+        wrapper_element, options
+        this might be the dumbest thing ever...
+    */
+    example = new Canvas( $("#wrapper"), {
+        width: 640,
+        height: 480,
+        autostart: true,
+        assets: ["img/worm.png"], // these are automatically loaded for us
+        draw: function(timestamp) {
+            this.background('rgb(245,245,245)');
+            for(var i = 0; i < 5; i++ ) {
+                this.food[i].draw(timestamp);    
+            }
+            this.images[0].draw(timestamp);
+            this.checkHits();
+            if( this.alive <= 0 ) {
+                this.context.fillStyle="#CC00FF";
+                this.context.fillText("you win!", 200, 200);
+            }
+
+            if( this.active ) {
+
+            } else {
+                this.context.fillStyle = "rgba(0,0,0,.78)";
+                this.context.fillRect(0,0,this.w,this.h);
+                this.context.fillStyle = "#FFFFFF";
+                this.context.fillText("Click to play!", 200, 200);
+            }
+        }
+    });
+
+    example.setup();
+
+    // sfx = new Audio("wav/sfx.wav");
+    // http://f1lt3r.com/w3caudio/web-audio-api/basic-examples/looping-sounds-without-gaps.html
+    audiocontext = new webkitAudioContext();
+    audiosource = audiocontext.createBufferSource();
+    var request = new XMLHttpRequest();
+    request.addEventListener( 'load', function(e) {
+        audiocontext.decodeAudioData( request.response, function(decoded_data) {
+            audiosource.buffer = decoded_data;
+            audiosource.connect( audiocontext.destination );
+            audiosource.loop = false;
+            audiosource.noteOn( 0 );
+        }, function(e){
+            console.log("error");
+        });
+    }, false);
+    request.open( 'GET', 'wav/sfx.wav', true );
+    request.responseType = "arraybuffer";
+    request.send();
+});
